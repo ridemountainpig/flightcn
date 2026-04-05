8ca5fef7f237d98bfd5eaac0bb9a4e84d7ab1de6
ridemountainpig <lin0905580802@gmail.com>
2026-04-05 23:29:23 +0800

    chore: add @next/third-parties and Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -78,6 +79,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-D5P23L59BL" />
         {children}
       </body>
     </html>
